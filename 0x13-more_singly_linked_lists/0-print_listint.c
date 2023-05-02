388015cddbcbc5f83b70faabdf5c1f4100af38bb
Oshua33 <oshuaaigbor@gmail.com>
2023-05-02 15:52:59 -0700

    1-listint_len.c file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listrint - linked list element to be printed
+ * print_listint - linked list element to be printed
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
@@ -18,5 +18,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 	}
 
-	return(num);
+	return (num);
 }
